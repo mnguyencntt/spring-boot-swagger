50e774f53e6a5a186544fbb190e0e425a3e055a7
MinhNguyen <MinhNguyen>
2020-07-27 23:30:28 +0800

    add PathVariable

--- src/main/java/com/anz/swagger/controller/ProductController.java
+++ src/main/java/com/anz/swagger/controller/ProductController.java
@@ -48,26 +48,26 @@ public class ProductController {
 
   @ApiOperation(value = "Add a product")
   @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
-  public ResponseEntity saveProduct(@RequestBody Product product) {
+  public ResponseEntity<String> saveProduct(@RequestBody Product product) {
     productService.saveProduct(product);
-    return new ResponseEntity("Product saved successfully", HttpStatus.OK);
+    return new ResponseEntity<String>("Product saved successfully", HttpStatus.OK);
   }
 
   @ApiOperation(value = "Update a product")
   @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = "application/json")
-  public ResponseEntity updateProduct(@PathVariable Integer id, @RequestBody Product product) {
+  public ResponseEntity<String> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
     Product storedProduct = productService.getProductById(id);
     storedProduct.setDescription(product.getDescription());
     storedProduct.setImageUrl(product.getImageUrl());
     storedProduct.setPrice(product.getPrice());
     productService.saveProduct(storedProduct);
-    return new ResponseEntity("Product updated successfully", HttpStatus.OK);
+    return new ResponseEntity<String>("Product updated successfully", HttpStatus.OK);
   }
 
   @ApiOperation(value = "Delete a product")
   @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
-  public ResponseEntity delete(@PathVariable Integer id) {
+  public ResponseEntity<String> delete(@PathVariable Integer id) {
     productService.deleteProduct(id);
-    return new ResponseEntity("Product deleted successfully", HttpStatus.OK);
+    return new ResponseEntity<String>("Product deleted successfully", HttpStatus.OK);
   }
 }
